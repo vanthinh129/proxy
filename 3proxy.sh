f7e086a614f95be53644a4fbe0898f192667334d
vanthinh129 <vanthinh291@gmail.com>
2023-06-09 19:24:07 +0700

    Update 3proxy.sh

--- 3proxy.sh
+++ 3proxy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/vanthinh129/proxy/raw/main/3proxy-3proxy-0.8.6.tar"
+    URL="https://raw.githubusercontent.com/vanthinh129/proxy/main/3proxy-3proxy-0.8.6.tar"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
